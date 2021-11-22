d3cc65f54676c5c113955feda26f627aac3a091b
JackPitcher <58702913+JackPitcher@users.noreply.github.com>
2021-11-21 22:20:17 -0800

    Added GET/PUT/DELETE server methods

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,16 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+const putUrl = "http://localhost:4321/dataset/:id/:kind"
+const datasetInput = document.getElementById('datasetInput');
+datasetInput.addEventListener("change", handleFiles, false);
 
-function handleClickMe() {
-	alert("Button Clicked!");
+async function handleFiles(){
+	let formData = new FormData();
+	formData.append("fileToUpload", datasetInput.files[0]);
+
+	const xhr = new XMLHttpRequest();
+	await xhr.open('PUT', putUrl, true);
+	xhr.onload = () => {
+		console.log("dataset updated");
+	};
+	xhr.send(formData);
+
 }
